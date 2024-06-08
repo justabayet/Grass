7465ebbd7ef61a74cee5673f304d3186dd929df2
justabayet <anthony.bayet@hotmail.com>
2024-06-08 22:51:47 +0200

    # Set ground color

--- src/App/Ground.tsx
+++ src/App/Ground.tsx
@@ -8,7 +8,7 @@ function Ground({ size = 5, ...props }: GroundProps): JSX.Element {
   return (
     <mesh {...props} rotation={[- Math.PI / 2, 0, 0]}>
       <boxGeometry args={[size, size, 1, 1]} />
-      <meshStandardMaterial color={0xfbabff} />
+      <meshStandardMaterial color={0xffb6f5} />
     </mesh>
   )
 }
