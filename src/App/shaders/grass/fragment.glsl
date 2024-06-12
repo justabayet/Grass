9c4c1ecb5523ad95d3b1b134628f9a01a51189a4
justabayet <anthony.bayet@hotmail.com>
2024-06-13 00:56:34 +0200

    + sun color in fragment

--- src/App/shaders/grass/fragment.glsl
+++ src/App/shaders/grass/fragment.glsl
@@ -2,12 +2,21 @@ uniform vec3 uBaseColor;
 uniform vec3 uTipColor;
 varying vec2 vUv;
 varying float vOscillation;
+uniform float sunInfluence;
+uniform float sunIntensity;
+uniform vec3 sunColor;
+uniform vec3 reflectionColor;
 
 void main()
 {
     vec3 color = mix(uBaseColor, uTipColor, vUv.y * vUv.y);
 
-    float mixOscillation = (vOscillation + 0.5) * 0.8; // [0;0.8]
-    gl_FragColor = vec4(color, 1.0 - mixOscillation);
+    color = mix(color, sunColor, sunIntensity);
+
+    float mixOscillation = pow((vOscillation + 0.5), 1.5) * sunInfluence; // [0;0.8]
+    color = mix(color, reflectionColor, mixOscillation);
+
+    gl_FragColor = vec4(color, 1.0);
+    // gl_FragColor = vec4(vec3(vOscillation + 0.5), 1.0);
     #include <colorspace_fragment>;
 }
